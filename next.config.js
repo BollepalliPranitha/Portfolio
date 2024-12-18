77ece498d3ad495c4fab67960fc5c311b7fe3c7c
Pranitha <pranithabollepalli20@gmail.com>
2024-12-17 19:12:07 -0600

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-
-
 module.exports = {
     reactStrictMode: true,
     env: {
       NAME: process.env.NAME,
     },
     output: 'export',
-  basePath: '/Portfolio', // Replace with your GitHub repo name
-  assetPrefix: '/Portfolio', 
-  assetPrefix: './', // Ensures assets (CSS, JS) are loaded correctly
+  basePath: '/Portfolio', 
+  assetPrefix: '', 
   trailingSlash: true,
 }
